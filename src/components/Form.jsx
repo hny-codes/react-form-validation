3d18dc374a9d4d5d1fcdd1ca35623e5be4a6af90
Henry <hnguyhenry@protonmail.com>
2023-04-13 21:05:24 -0700

    Add form elements to Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,65 @@
 import './Form.css';
+import { useState } from 'react';
+import {
+  EmailElement,
+  NumberElement,
+  PasswordElement,
+  TextElement,
+} from './FormElements';
 
 const Form = ({ title }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [country, setCountry] = useState('');
+  const [zipCode, setZipCode] = useState(0);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   return (
-    <div className="form">
+    <form className='form'>
       <h1 className='title'>{title}</h1>
-      <p>Name: </p>
-      <p>Email: </p>
-      <p>Country: </p>
-      <p>Zip Code:</p>
-      <p>Password:</p>
+      <TextElement
+        text={'Name'}
+        name={'name'}
+        value={name}
+        handleValue={setName}
+        id={'name'}
+        required={true}
+      />
+      <EmailElement
+        text={'Email'}
+        name={'email'}
+        value={email}
+        handleValue={setEmail}
+        id={'email'}
+        required={true}
+      />
+      <TextElement
+        text={'Country'}
+        name={'country'}
+        value={country}
+        handleValue={setCountry}
+        id={'country'}
+        required={true}
+      />
+      <NumberElement
+        text={'Zip Code'}
+        name={'zip'}
+        value={zipCode}
+        handleValue={setZipCode}
+        id={'zip'}
+        required={true}
+      />
+      <PasswordElement
+        text={'Password'}
+        name={'password'}
+        value={password}
+        handleValue={setPassword}
+        id={'password'}
+        required={true}
+      />
       <p>Confirm Password: </p>
-    </div>
+    </form>
   );
 };
 
